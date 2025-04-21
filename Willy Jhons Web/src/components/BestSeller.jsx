d55c640696f56a5e044197f595e2f07e3d735536
Gabriel Quintero <jesusblanco091@gmail.com>
2025-04-21 06:21:19 -0400

    Agregando archivo productData.js y modificacion a BestSeller.jsx

--- Willy Jhons Web/src/components/BestSeller.jsx
+++ Willy Jhons Web/src/components/BestSeller.jsx
@@ -1,99 +1,35 @@
-import '../css/styles_main_screen_best_seller.css'
+import "../css/styles_main_screen_best_seller.css";
+import data from "../data/bestSellerData";
 export const BestSeller = () => {
   return (
     <>
-    
-    <h2>Lo más vendido</h2>
-    
-    <div className="containerBestSeller">
-    <div className="container-product0">
-        <img src="/src/assets/products/anilloDelLeon.jpg" alt="" className='product'/>
-       <table>
-        <thead>
-        <tr>
-          <th className='productName'>Anillo del Leon</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-          <td className='productDescription'>Acero Inoxidable </td>
-        </tr>
-        </tbody>
-        <tbody>
-        <tr>
-        <td className='productDescription'>Baño de oro</td>
-        <td className='productPrice'>8.99$</td>
-        </tr>
-        </tbody>
-       </table>
-    </div>
-    <div className="container-product1">
-        <img src="/src/assets/products/anilloPlataOro.jpg" alt=""  className='product'/>
-        <table>
-          <thead>
-        <tr>
-          <th className='productName'>Anillo plata y oro</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-          <td className='productDescription'>Acero Inoxidable </td>
-        </tr>
-        </tbody>
-        <tbody>
-        <tr>
-        <td className='productDescription'>Baño de oro</td>
-        <td className='productPrice'>19.99$</td>
-        </tr>
-        </tbody>
-       </table>
-       
-    </div>
-    <div className="container-product2">
-        <img src="/src/assets/products/anilloCorazon.jpg" alt=""  className='product'/>
-        <table>
-          <thead>
-        <tr>
-          <th className='productName'>Anillo corazón</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-          <td className='productDescription'>Anillo de plata</td>
-        </tr>
-        </tbody>
-        <tbody>
-        <tr>
-        <td className='productDescription'>Piedras incrustradas</td>
-        <td className='productPrice'>29.99$</td>
-        </tr>
-        </tbody>
-       </table>
-      
-    </div>
-    <div className="container-product3">
-        <img src="/src/assets/products/anilloColores.jpg" alt=""  className='product'/>
-        <table>
-          <thead>
-        <tr>
-          <th className='productName'>Anillos varios</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-          <td className='productDescription'>Acero Inoxidable </td>
-        </tr>
-        </tbody>
-        <tbody>
-        <tr>
-        <td className='productDescription'>Piedra incrustradas</td>
-        <td className='productPrice'>9.99$</td>
-        </tr>
-        </tbody>
-       </table>
-       
-    </div>
-    </div>
+      <h2>Lo más vendido</h2>
+
+      <div className="containerBestSeller">
+        {data.map((item) => (
+          <div key={item.id} className="containerProducts">
+            <img src={item.image} alt="" className="product"/>
+            <table>
+              <thead>
+                <tr>
+                  <th className="productName">{item.name}</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className="productDescription">{item.description0} </td>
+                </tr>
+              </tbody>
+              <tbody>
+                <tr>
+                  <td className="productDescription">{item.description1}</td>
+                  <td className="productPrice">{item.price}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        ))}
+      </div>
     </>
-  )
-}
+  );
+};
---
+++ Willy Jhons Web/src/data/bestSellerData.js
@@ -0,0 +1,36 @@
+const bestSellerData = [
+  {
+    id: 1,
+    name: "Anillo del Leon",
+    image: "/src/assets/products/anilloDelLeon.jpg",
+    description0: "Acero inoxidable",
+    description1: "Baño de oro",
+    price: "8.99$",
+  },
+
+  {
+    id: 2,
+    name: "Anillo Plata y Oro",
+    image:  "/src/assets/products/anilloPlataOro.jpg",
+    description0: "Acero inoxidable",
+    description1: "Baño de oro",
+    price: "19.99$",
+  },
+
+  {id: 3,
+    name: 'Anillo corazón',
+    image: "/src/assets/products/anilloCorazon.jpg",
+    description0: 'Anillo de plata',
+    description1: 'Piedras incrustradas',
+    price: '29.99$'
+    },
+  {id: 4,
+    name: 'Anillos varios',
+    image:  "/src/assets/products/anilloColores.jpg", 
+    // '../assets/products/anilloColores.jpg',
+    description0: 'Acero inoxidable',
+    description1: 'Piedras incrustradas',
+    price: '9.99$'
+    },
+];
+export default bestSellerData
